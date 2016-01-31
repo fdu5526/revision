d9c34d7dc68ee775883aec9c5f9093d7328ff94f
Thomas Lu <thomastianlu@gmail.com>
2016-01-31 13:14:17 -0800

    Gonna merge attempt

---
+++ Assets/WordClickScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WordClickScript : MonoBehaviour {
+
+    [SerializeField]
+    private int _wordStringCount;
+
+	// Use this for initialization
+	void Awake () {
+        _wordStringCount = GetComponent<TextMesh>().text.Length;
+        GetComponent<BoxCollider>().size = new Vector3(_wordStringCount*5 / 8, 1f, 1f);
+	}
+
+    void OnMouseDown() {
+        Debug.Log("HELLO I WILL TRANSFER");
+    }
+}
