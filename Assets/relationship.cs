8b79f599e5857a64d57f20c3186b471be31febc3
Steve Cha <hyungs.cha@gmail.com>
2016-01-31 13:02:53 -0800

    MASSIVE COMMIT
    I had to redo things. T_T

---
+++ Assets/relationship.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class relationship {
+
+    public string name;
+    public bool like;
+    public string opinion;
+
+    public relationship(string theName, bool likes, string theOpinion)
+    {
+        name = theName;
+        like = likes;
+        opinion = theOpinion;
+    }       
+}
