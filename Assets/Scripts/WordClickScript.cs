1459821b5386ba4c1634862455adfb6edde86685
Thomas Lu <thomastianlu@gmail.com>
2016-02-07 14:14:32 -0800

    New Script

--- Assets/Scripts/WordClickScript.cs
+++ Assets/Scripts/WordClickScript.cs
@@ -10,13 +10,14 @@ public class WordClickScript : MonoBehaviour {
     private GameObject _UIPrefab;
     [SerializeField]
     private GameObject _UIParent;
-   
+
 
 	// Use this for initialization
 	void Awake () {
         _wordStringCount = GetComponent<TextMesh>().text.Length;
         GetComponent<BoxCollider>().size = new Vector3(_wordStringCount*5 / 8, 1f, 1f);
-	}
+        _UIParent = GameObject.Find("UIWordPanel");
+    }
 
     public void Initialize(GameObject UIParent) {
         _UIParent = UIParent;
@@ -24,9 +25,8 @@ public class WordClickScript : MonoBehaviour {
     
 
     void OnMouseDown() {
-        Debug.Log("HELLO I WILL TRANSFER");
         GameObject UIObj = Instantiate(_UIPrefab, transform.position, transform.rotation) as GameObject;
         UIObj.GetComponent<TextUIObject>().Initialize(GetComponent<TextMesh>().text);
-        UIObj.transform.parent = GameObject.Find("UIWordPanel").transform;
+        UIObj.transform.parent = _UIParent.transform;
     }
 }
