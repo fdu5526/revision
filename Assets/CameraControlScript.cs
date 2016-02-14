efdaa7612a76dc41d94b203ed2e9192623f5f06e
Thomas Lu <thomastianlu@gmail.com>
2016-02-14 14:22:34 -0800

    Added new player controller!

---
+++ Assets/CameraControlScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControlScript : MonoBehaviour {
+
+    private float _inputX;
+    private float _inputY;
+
+    [SerializeField]
+    private float _cameraRotationSpeed = 2;
+    [SerializeField]
+    private float _cameraMoveSpeed = 2;
+
+    [SerializeField]
+    private GameObject _objectOfInterest;
+    [SerializeField]
+    private GameObject _playerObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageMouseMovement();
+        ManageCameraPosition();
+	}
+
+    void ManageMouseMovement() {
+        if (_objectOfInterest == null) { 
+            _inputX = Input.GetAxis("Mouse X");
+            _inputY = Input.GetAxis("Mouse Y");
+
+            transform.localEulerAngles += new Vector3(_inputY, _inputX, 0f) * _cameraRotationSpeed;
+        }
+    }
+
+    void ManageCameraPosition() {
+        if (_objectOfInterest != null) { 
+            transform.position = Vector3.MoveTowards(transform.position, _objectOfInterest.transform.position, Time.deltaTime * _cameraMoveSpeed);
+        }
+        else {
+            transform.position = _playerObject.transform.position;
+        }
+    }
+}
---
+++ Assets/FollowCameraPivot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCameraPivot : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _cameraPivot;
+    [SerializeField]
+    private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion dummyRotation = _cameraPivot.transform.rotation;
+        dummyRotation.x = 0;
+        dummyRotation.z = 0;
+        transform.rotation = dummyRotation;
+        transform.position = _player.transform.position;
+	}
+}
