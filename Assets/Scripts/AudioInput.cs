2cf43cabb6d16c8dfd2db23ca3a28a882d70a318
Dave Yan <fdu5526@live.com>
2016-03-09 10:37:57 -0800

    added logging to more scene files, made it so that microphone listener moves on after 8 seconds if no matter input

--- Assets/Scripts/AudioInput.cs
+++ Assets/Scripts/AudioInput.cs
@@ -5,28 +5,43 @@ public class AudioInput : MonoBehaviour {
 
 	public GameObject activeOnTrigger;
 
-    void Start () {
-        string[] devs = Microphone.devices;
+  float maxTimePassToMoveOn = 8f;
+  float timeStart;
+
+  void Start () {
+    string[] devs = Microphone.devices;
 		print(devs.Length);
-        GetComponent<AudioSource>().clip = Microphone.Start(devs[0], true, 10, 44100);
-    }
+    GetComponent<AudioSource>().clip = Microphone.Start(devs[0], true, 10, 44100);
+    timeStart = Time.time;
+  }
 
 
-    void Update () {
-            AudioSource aud = GetComponent<AudioSource>();
-            float[] samples = new float[aud.clip.samples * aud.clip.channels];
-            aud.clip.GetData(samples, 0);
-            int i = 0;
-           
-            while (i < samples.Length) {
-              //  Debug.Log(samples[i]);
-                 if(samples[i]>0.2f){
-                 //Debug.Log("The audio has been triggered and the number is: "+i);
-				activeOnTrigger.SetActive(true);
-					Destroy(gameObject);
-                 }
-                ++i;
-            }
-            aud.Play();
+  void MoveOn () {
+    activeOnTrigger.SetActive(true);
+    Destroy(gameObject);
+  }
+
+  void Update () {
+    if (Time.time - timeStart > maxTimePassToMoveOn) {
+      MoveOn();
+      return;
     }
+
+    AudioSource aud = GetComponent<AudioSource>();
+    float[] samples = new float[aud.clip.samples * aud.clip.channels];
+    aud.clip.GetData(samples, 0);
+    int i = 0;
+   
+    while (i < samples.Length) {
+      
+      if(samples[i]>0.2f) {
+        Debug.Log(samples[i]);
+        //Debug.Log("The audio has been triggered and the number is: "+i);
+        MoveOn();
+        break;
+      }
+      ++i;
+    }
+    aud.Play();
+  }
 }
