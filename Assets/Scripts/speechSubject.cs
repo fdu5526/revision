3c7eac4e892a2d936950e3b230f3633a356ea2b1
jsohn96 <jsohn96@hotmail.com>
2016-03-20 15:19:59 -0700

    UIFungus prefab change
    So that it is inside the character

--- Assets/Scripts/speechSubject.cs
+++ Assets/Scripts/speechSubject.cs
@@ -4,33 +4,46 @@ using System.Collections;
 	
 public class speechSubject : MonoBehaviour {
 	GameObject uiElement;
+	Text uiCharacterText;
 	Text uiText;
 //	MeshRenderer mr; 
 //	TextMesh tm;
 	Vector3 speechBubbleSize;
 //	Transform uiBubble;
 	Text uiBubbleText;
+
 	[SerializeField] GameObject Bubbles;
+	[SerializeField] Image bubbleImage;
+	[SerializeField] Image bubbleTail;
 	RectTransform UIBubble;
 	RectTransform bubbleSize;
 
+	[SerializeField] string characterCheck;
+	//for disabling/enabling speech bubble
+
+
+	//TODO: Look at Camera (Billboard)
+	//TODO: Copy Steve's Position
 
 	// Use this for initialization
 	void Awake () {
 		uiElement = GameObject.Find("StoryText");
+		uiCharacterText = GameObject.Find("NameText").GetComponent <Text>();
 		uiText = uiElement.GetComponent<Text> ();
 
 //		mr = gameObject.GetComponent<MeshRenderer> ();
 //		uiBubble = gameObject.transform.GetChild (0);
 //		tm = gameObject.GetComponent<TextMesh> ();
 		uiBubbleText = gameObject.GetComponent<Text> ();
+
 		UIBubble = Bubbles.transform.GetComponent<RectTransform> ();
 		bubbleSize = gameObject.GetComponent <RectTransform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (uiText.IsActive()) {
+		if (uiText.IsActive() && (uiCharacterText.text == characterCheck || uiCharacterText.text == "")) {
+			enableBubble ();
 //			tm.text = uiText.text;
 			//UI TEXT
 			if (uiBubbleText.text != uiText.text) {
@@ -40,10 +53,10 @@ public class speechSubject : MonoBehaviour {
 
 		} else {
 //			tm.text = "";
-
 			//UI TEXT
 			uiBubbleText.text = "";
-			gameObject.transform.parent.gameObject.SetActive (false);
+			disableBubble ();
+//			gameObject.transform.parent.gameObject.
 //			resizeBubs ();
 		}
 	}
@@ -56,6 +69,18 @@ public class speechSubject : MonoBehaviour {
 		float bubbleOffset = UIBubble.sizeDelta.y / 2f + 5f;
 		UIBubble.localPosition = new Vector3 (0f, bubbleOffset, 0f);
 		bubbleSize.localPosition = new Vector3 (0f, bubbleOffset, 0f);
+	}
+
+	void disableBubble() {
+		uiBubbleText.enabled = false;
+		bubbleTail.enabled = false;
+		bubbleImage.enabled = false;
+	}
+
+	void enableBubble() {
+		uiBubbleText.enabled = true;
+		bubbleTail.enabled = true;
+		bubbleImage.enabled = true;
 	}
 //	public void resizeBubs() { 
 //		if (mr.bounds.extents.x != 0) {
