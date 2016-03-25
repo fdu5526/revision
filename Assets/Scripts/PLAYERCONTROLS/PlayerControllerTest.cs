3ab419bbbf6381745acc239b79d54025630ac177
Allison Comrie <ajcomrie@usc-secure-wireless-077-039.usc.edu>
2016-03-25 14:29:37 -0700

    Merge branch 'master' into Allison_UI
    # Conflicts:
    #	ProjectSettings/GraphicsSettings.asset

--- Assets/Scripts/PLAYERCONTROLS/PlayerControllerTest.cs
+++ Assets/Scripts/PLAYERCONTROLS/PlayerControllerTest.cs
@@ -11,6 +11,7 @@ public class PlayerControllerTest : MonoBehaviour {
     private float _moveForward;
     private float _moveSide;
 	public static bool movementEnabled = true;
+    private float _velocity;
 
     private Vector3 _forwardPosition;
 
@@ -27,16 +28,66 @@ public class PlayerControllerTest : MonoBehaviour {
 		}
     }
 
+    public float ReturnVelocity() {
+        return _velocity;
+    }
+
     void ManageMovement() {
         _forwardPosition = _camera.transform.forward;
         _moveForward = Input.GetAxis("Vertical");
         _moveSide = Input.GetAxis("Horizontal");
 
         transform.position += ((_camera.transform.forward * _moveForward) + (_camera.transform.right *_moveSide)) *_moveSpeed;
-		
+
+        _velocity = (((_camera.transform.forward * _moveForward) + (_camera.transform.right * _moveSide)) * _moveSpeed).magnitude;
     }
 
     void ManageRotation() {
-        transform.rotation = _camera.transform.rotation;
+        
+        // Horizontal Facing
+        if (_moveForward == 0) { 
+            if (_moveSide < 0)
+            {
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 0f, 0f));
+            }
+            else if (_moveSide > 0){
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 180f, 0f));
+            }
+        }
+
+        // Vertical Facing && Diagonal Facing
+        if (_moveForward < 0)
+        {
+            if (_moveSide ==  0) { 
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 270f, 0f));
+            }
+            else if (_moveSide > 0)
+            {
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 225f, 0f));
+            }
+            else if (_moveSide < 0)
+            {
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 315f, 0f));
+            }
+        }
+        else if (_moveForward > 0)
+        {
+            if (_moveSide == 0) { 
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 90f, 0f));
+            }
+
+            else if (_moveSide > 0)
+            {
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 135f, 0f));
+            }
+
+            else if (_moveSide < 0)
+            {
+                transform.rotation = Quaternion.Euler(_camera.transform.localEulerAngles + new Vector3(0f, 45f, 0f));
+            }
+        }
+
+
+
     }
 }
