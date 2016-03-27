2802e36093916005fe8790796ab17a91dfeb6fad
jsohn96 <jsohn96@hotmail.com>
2016-03-27 04:18:19 -0700

    Billboard speeches

---
+++ Assets/Scripts/speechBillboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speechBillboard : MonoBehaviour {
+	Transform camera;
+	[SerializeField] bool flip;
+
+	void Awake () {
+		camera = Camera.main.transform;
+	}
+
+	void Update () {
+		Vector3 lookAtCamera = (camera.position - transform.position);
+		lookAtCamera.y = 0;
+		//keep the x coordinate still
+//		lookAtCamera.x = transform.position.x;
+		transform.forward = flip? -lookAtCamera : lookAtCamera;
+	}
+}
