3ada98b5bfe10bb2314f67e4e681ea7195cabbcb
Thomas Lu <thomastianlu@gmail.com>
2016-04-03 14:52:21 -0700

    Transformation animation in progress

---
+++ Assets/PerpetualRotate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerpetualRotate : MonoBehaviour {
+
+    private enum RotateAxis {
+        X,
+        Y,
+        Z
+    }
+
+    [SerializeField]
+    private RotateAxis _rotateAxis;
+    [SerializeField]
+    private float _speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    switch (_rotateAxis) {
+            case RotateAxis.X:
+                transform.Rotate(new Vector3(1f, 0f, 0f) * _speed);
+                break;
+            case RotateAxis.Y:
+                transform.Rotate(new Vector3(0f, 1f, 0f) * _speed);
+                break;
+            case RotateAxis.Z:
+                transform.Rotate(new Vector3(0f, 0f, 1f) * _speed);
+                break;
+        }
+	}
+}
