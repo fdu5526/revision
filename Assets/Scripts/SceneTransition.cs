bd0b91eb1db112618ed59e36023663a945211e88
Dave Yan <fdu5526@gmail.com>
2016-05-01 21:54:16 -0700

    removed scene transitions for 2nd time around

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -23,6 +23,10 @@ public class SceneTransition : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
+		if (gameProgress.secondTime) { // do nothing 2nd time around
+			this.enabled = false;
+			return;
+		}
 		defaultStevePosition = transform.position;
 		defaultSteveScale = transform.localScale;
 		currentState = State.Start;
