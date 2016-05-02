b166294f27fdcfc66038035ce2fb9f8fe09ee41c
Thomas Lu <thomastianlu@gmail.com>
2016-05-01 18:17:22 -0700

    Mom mini game fixed and good to go!
    Needs to polish some art but the guts are there

---
+++ Assets/MomOutsideDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MomOutsideDetection : MonoBehaviour {
+
+    [SerializeField]
+    private MomDetector _momDetector;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "Mom") {
+            _momDetector.SetMomState(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Mom")
+        {
+            _momDetector.SetMomState(false);
+        }
+    }
+}
