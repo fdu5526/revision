bead3e898f3c36576008272ede1052fb6a859143
Steve Cha <Steve Cha>
2016-05-05 03:08:16 -0700

    audio fixed, transitions smoother, just before trying to add more animations...

---
+++ Assets/DestroyAll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAll : MonoBehaviour {
+
+	public string TypeToDestroy;
+	public GameObject[] Destructee;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DestroyTagged(){
+		Destructee = GameObject.FindGameObjectsWithTag (TypeToDestroy);
+		foreach (GameObject GO in Destructee) {
+			Destroy (GO);
+		}
+	}
+}
